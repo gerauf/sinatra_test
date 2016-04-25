0d696b1e9b1b37a5e37a58e67f82555fa68bd52f
gerauf <fo4786@gmail.com>
2016-04-25 17:57:28 +0100

    dynamic content

--- app.rb
+++ app.rb
@@ -8,29 +8,18 @@ end
 get '/secret' do
   "Victorias....shhhh"
 end
-get '/secret1' do
-  "shiiiiit"
-end
-get '/secret2' do
-  "wassup"
-end
-get '/secret3' do
-  "ouch"
-end
-get '/secret4' do
-  "o.m.g"
-end
-get '/secret5' do
-  "bam"
-end
 
-get '/cat' do
+get '/random-cat' do
   @action = ["do", "eat", "walk", "stroke", "lick"].sample
   @pronoun = ["my", "your", "our", "their"].sample
   @noun = ["homework", "cheese", "exercises", "melons"].sample
   erb :index
 end
 
-get '/cat/dog' do
-  'catdog' #http://bit.ly/1eze8aE
+get '/named-cat' do
+  @action = params[:action]
+  @pronoun = params[:pronoun]
+  @noun = params[:noun]
+  puts params
+  erb :index
 end
